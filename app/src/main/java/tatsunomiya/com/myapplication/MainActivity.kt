6f67bcdf054795b312fa5ff9c0355efe9c9e499b
Tatsunomiya <black.r.d@icloud.com>
2019-11-21 00:47:51 +0900

    //  どうやってHumanインスタンスを二個作って、 関数hobbyを実行しようか？         //  Humanクラスのコンストラクタはname とageしか入れる事しかできませんでした。
    //        2019-11-20 23:51:27.464 14631-14631/tatsunomiya.com.myapplication D/tasunomiya: 私の名前は鈴木です
    //        2019-11-20 23:51:27.464 14631-14631/tatsunomiya.com.myapplication D/tatshunomiya: 私の歳は25です
    //        2019-11-20 23:51:27.464 14631-14631/tatsunomiya.com.myapplication D/tatshunomiya: 私はkotlin.jvm.internal.StringCompanionObject@b877e05について考える)
    //        2019-11-20 23:51:27.465 14631-14631/tatsunomiya.com.myapplication D/tatshunomiya: 私はkotlin.jvm.internal.StringCompanionObject@b877e05について考える)
    
            //Humanクラスにhobbyを引数に渡したコンストラクタが必要なのはわかるのですが、どう作ればいいのかもうわかないので質問します

--- app/src/main/java/tatsunomiya/com/myapplication/MainActivity.kt
+++ app/src/main/java/tatsunomiya/com/myapplication/MainActivity.kt
@@ -10,12 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         val human = Human("鈴木" ,25)
  //  どうやってHumanインスタンスを二個作って、 関数hobbyを実行しようか？
         //  Humanクラスのコンストラクタはname とageしか入れる事しかできませんでした。
-        
+
+//        2019-11-20 23:51:27.464 14631-14631/tatsunomiya.com.myapplication D/tasunomiya: 私の名前は鈴木です
+//        2019-11-20 23:51:27.464 14631-14631/tatsunomiya.com.myapplication D/tatshunomiya: 私の歳は25です
+//        2019-11-20 23:51:27.464 14631-14631/tatsunomiya.com.myapplication D/tatshunomiya: 私はkotlin.jvm.internal.StringCompanionObject@b877e05について考える)
+//        2019-11-20 23:51:27.465 14631-14631/tatsunomiya.com.myapplication D/tatshunomiya: 私はkotlin.jvm.internal.StringCompanionObject@b877e05について考える)
+
+        //Humanクラスにhobbyを引数に渡したコンストラクタが必要なのはわかるのですが、どう作ればいいのかもうわかないので質問します
+
 
 
         human.say()
